d64734c2f3e0bec21d44a62d360bfbb9fca524b9
Sean Yager <contact@seanyager.com>
2021-01-21 18:22:51 -0600

    deleted boilerplate, began working on characterCanvas Component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CharacterCanvas />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CharacterCanvas from "@/components/CharacterCanvas"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CharacterCanvas,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>

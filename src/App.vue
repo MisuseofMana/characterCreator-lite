61619083b413b8f90d315de1c39a1777e11b7608
Sean Yager <contact@seanyager.com>
2021-01-22 18:48:57 -0600

    added in viewport centering

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
+    <h1>MOEBIUS DUNGEONS ADVENTURER CREATOR</h1>
     <CharacterCanvas />
   </div>
 </template>
 
 <script>
+import '@/css/fonts.css'
+
 import CharacterCanvas from "@/components/CharacterCanvas"
 
 export default {
@@ -17,6 +20,11 @@ export default {
 
 <style>
 #app {
-
+  width:100%;
+  height:100%;
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>

beaab3d7883095c38907208d50e3f7d3542fbf61
Sean Yager <contact@seanyager.com>
2021-01-23 01:58:23 -0600

    added fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faDice, faArrowUp, faArrowLeft, faArrowRight, faArrowDown, faCircle, faAngleDoubleDown, faAngleDoubleUp, faAngleDoubleLeft, faAngleDoubleRight, faSync } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faArrowUp)
+library.add(faArrowDown)
+library.add(faArrowRight)
+library.add(faArrowLeft)
+library.add(faCircle)
+library.add(faAngleDoubleRight)
+library.add(faAngleDoubleUp)
+library.add(faAngleDoubleDown)
+library.add(faAngleDoubleLeft)
+library.add(faSync)
+library.add(faDice)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({

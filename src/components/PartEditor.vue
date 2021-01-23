6fe9ba125a3e90a1e8ca3dcd04460e1cea66dea7
Sean Yager <contact@seanyager.com>
2021-01-23 01:59:22 -0600

    new classes - redesign of repositioning ui

--- src/components/PartEditor.vue
+++ src/components/PartEditor.vue
@@ -2,18 +2,18 @@
     <div class="border">
         <h2>You are editing the {{ type }}.</h2>
 
-        <div class="flexRow">
+        <div class="flexRow section">
             <button @click="$emit('disable-item', 'disable')">NO {{ type.toUpperCase() }}</button>
         </div>
 
-        <div class="flexRow">
-            <p>Selection</p>
-            <input type="range" min="0" :max="maxRange" v-model="value" class="slider" id="myRange">
+        <div class="flexRow section">
+            <p>SELECTION</p>
+            <input type="range" min="0" :max="maxRange-1" v-model="value" class="slider" id="myRange">
             <p>Option</p>
             <p>{{ value }}</p>
         </div>
 
-        <div class="flexRow">
+        <div class="flexRow section">
             <p>COLORS</p>
             <div @click="colorize('none')" class="dotborder"><p>NONE</p></div>
             <div @click="colorize('red')" class="red dot"></div>
@@ -28,22 +28,111 @@
 
         </div>
 
-        <div class="flexRow">
-            <p>Move Up or Down</p>
-            <button @click="reposition(-1, 'y')">UP 1px</button>
-            <button @click="reposition(1, 'y')">DOWN 1px</button>
-            <p>|</p>
-            <button @click="reposition(-5, 'y')">UP 5px</button>
-            <button @click="reposition(5, 'y')">DOWN 5px</button>
+        <div class="flexRow section">
+            <p>Reposition:</p>
+            <div class="flexColumn">
+                <div class="flexRow">
+                    <div class="border margin arrow" @click="reposition(-1, 'xy-neg')">
+                        <font-awesome-icon class="fa-rotate-n45" :icon="['fas', 'arrow-up']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(-1, 'y')">
+                        <font-awesome-icon :icon="['fas', 'arrow-up']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(-1, 'xy-pos')">
+                        <font-awesome-icon class="fa-rotate-45" :icon="['fas', 'arrow-up']" />
+                    </div>
+                </div>
+
+                <div class="flexRow">
+                    <div class="border margin arrow" @click="reposition(-1, 'x')">
+                        <font-awesome-icon :icon="['fas', 'arrow-left']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(null, 'reset')">
+                        <font-awesome-icon class="fa-rotate-45" :icon="['fas', 'circle']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(1, 'x')">
+                        <font-awesome-icon :icon="['fas', 'arrow-right']" />
+                    </div>
+                </div>
+                
+                <div class="flexRow">
+                    <div class="border margin arrow" @click="reposition(1, 'xy-pos')">
+                        <font-awesome-icon class="fa-rotate-45" :icon="['fas', 'arrow-down']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(1, 'y')">
+                        <font-awesome-icon :icon="['fas', 'arrow-down']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(1, 'xy-neg')">
+                        <font-awesome-icon class="fa-rotate-n45" :icon="['fas', 'arrow-down']" />
+                    </div>
+                </div>
+            </div>
+            
+            <div class="line"></div>
+
+            <div class="flexColumn">
+                <div class="flexRow">
+                    <div class="border margin arrow" @click="reposition(-5, 'xy-neg')">
+                        <font-awesome-icon class="fa-rotate-n45" :icon="['fas', 'angle-double-up']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(-5, 'y')">
+                        <font-awesome-icon :icon="['fas', 'angle-double-up']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(-5, 'xy-pos')">
+                        <font-awesome-icon class="fa-rotate-45" :icon="['fas', 'angle-double-up']" />
+                    </div>
+                </div>
+
+                <div class="flexRow">
+                    <div class="border margin arrow" @click="reposition(-5, 'x')">
+                        <font-awesome-icon :icon="['fas', 'angle-double-left']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(null, 'reset')">
+                        <font-awesome-icon class="fa-rotate-45" :icon="['fas', 'circle']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(5, 'x')">
+                        <font-awesome-icon :icon="['fas', 'angle-double-right']" />
+                    </div>
+                </div>
+                
+                <div class="flexRow">
+                    <div class="border margin arrow" @click="reposition(5, 'xy-pos')">
+                        <font-awesome-icon class="fa-rotate-45" :icon="['fas', 'angle-double-down']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(5, 'y')">
+                        <font-awesome-icon :icon="['fas', 'angle-double-down']" />
+                    </div>
+                    <div class="border margin arrow" @click="reposition(5, 'xy-neg')">
+                        <font-awesome-icon class="fa-rotate-n45" :icon="['fas', 'angle-double-down']" />
+                    </div>
+                </div>
+            </div>
+           
+            <!-- <div class="line"></div> -->
+
+            <!-- <div class="flexColumn">
+                <div class="border flexRow rotate arrow">
+                    <p>Rotate 15</p>
+                    <font-awesome-icon class="fa-rotate-n45" :icon="['fas', 'sync']" />
+                </div>
+                <div class="border flexRow rotate arrow">
+                    <p>Rotate 45</p>
+                    <font-awesome-icon class="fa-rotate-n45" :icon="['fas', 'sync']" />
+                </div>
+                <div class="border flexRow rotate arrow">
+                    <p>Rotate 90</p>
+                    <font-awesome-icon class="fa-rotate-n45" :icon="['fas', 'sync']" />
+                </div>
+            </div> -->
         </div>
-        
+
         <div class="flexRow">
-            <p>Move Left or Right</p>
-            <button @click="reposition(-1, 'x')">LEFT 1px</button>
-            <button @click="reposition(1, 'x')">RIGHT 1px</button>
-            <p>|</p>
-            <button @click="reposition(-5, 'x')">LEFT 5px</button>
-            <button @click="reposition(5, 'x')">RIGHT 5px</button>
+            <div class="border margin arrow" @click="randomizeCharacter">
+                <font-awesome-icon :icon="['fas', 'dice']" />
+            </div>
+            <div class="border margin arrow" @click="resetCharacter">
+                <font-awesome-icon :icon="['fas', 'sync']" />
+            </div>
         </div>
     </div>
 </template>
@@ -67,7 +156,13 @@
             },
             colorize(color) {
                 this.$emit('change-color', color)
-            }
+            },
+            randomizeCharacter() {
+                this.$emit('randomize-character');
+            },
+            resetCharacter() {
+                this.$emit('reset-character');
+            },
         },
         mounted() {
             this.value = this.selected;
@@ -77,6 +172,28 @@
 </script>
 
 <style scoped>
+    .fa-rotate-45 {
+        -webkit-transform: rotate(45deg);
+        -moz-transform: rotate(45deg);
+        -ms-transform: rotate(45deg);
+        -o-transform: rotate(45deg);
+        transform: rotate(45deg);
+    }
+    .fa-rotate-n45 {
+        -webkit-transform: rotate(-45deg);
+        -moz-transform: rotate(-45deg);
+        -ms-transform: rotate(-45deg);
+        -o-transform: rotate(-45deg);
+        transform: rotate(-45deg);
+    }
+    
+    .rotate {
+        margin:10px 0;
+    }
+
+    .rotate p {
+        font-size:10px;
+    }
 
     p {
         margin-right:10px;
@@ -91,12 +208,42 @@
         display:flex;
         flex-direction: row;
         align-items:center;
+    }
+
+    .section {
         margin:10px 0;
     }
 
     .border {
         border:solid 1px black;
         padding:10px;
+    }
+
+    .margin {
+        margin:5px;
+        width:15px;
+        height:15px;
+        display:flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .line {
+        width:1px;
+        height:150px;
+        margin:0 10px;
+        border-right:solid black 2px;
+    }
+
+    .arrow {
+        cursor:pointer;
+        transition: all .1s;
+    }
+
+    .arrow:hover {
+        transform:scale(1.1);
+        background:rgb(136, 255, 215);
+        transition: all .1s;
     }
 
     .slidecontainer {

bf73ae5c45f613e4cc5f65f9962a49df3a2694e8
Sean Yager <contact@seanyager.com>
2021-01-30 00:20:23 -0600

    imported bootstrap media breakpoints

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,17 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "~bootstrap/scss/_functions.scss";
+@import "~bootstrap/scss/_variables.scss";
+@import "~bootstrap/scss/mixins/_breakpoints.scss";
+
+@include media-breakpoint-up(xs) {  }
+@include media-breakpoint-up(sm) {  }
+@include media-breakpoint-up(md) {  }
+@include media-breakpoint-up(lg) {  }
+@include media-breakpoint-up(xl) {  }
+
 #app {
   margin-top:15px;
 }
@@ -36,6 +46,7 @@ export default {
   margin-bottom:30px;
 }
 
+@include media-breakpoint-up(xs) {
 .tiny {
   font-size:.6vw;
   align-self: auto;
@@ -49,4 +60,68 @@ export default {
 h1.responsiveFont {
   font-size:2vw;
 }
+}
+@include media-breakpoint-up(sm) {
+.tiny {
+  font-size:1.3vw;
+  align-self: auto;
+  margin:5px;
+}
+
+.icon {
+  font-size:3vw;
+}
+
+h1.responsiveFont {
+  font-size:3vw;
+}
+}
+@include media-breakpoint-up(md) { 
+.tiny {
+  font-size:.9vw;
+  align-self: auto;
+  margin:5px;
+}
+
+.icon {
+  font-size:2vw;
+}
+
+h1.responsiveFont {
+  font-size:2vw;
+}
+}
+@include media-breakpoint-up(lg) {
+.tiny {
+  font-size:.8vw;
+  align-self: auto;
+  margin:5px;
+}
+
+.icon {
+  font-size:1.5vw;
+}
+
+h1.responsiveFont {
+  font-size:2.4vw;
+}
+}
+@include media-breakpoint-up(xl) {
+.tiny {
+  font-size:.9vw;
+  align-self: auto;
+  margin:5px;
+}
+
+.icon {
+  font-size:1vw;
+}
+
+h1.responsiveFont {
+  font-size:2vw;
+}
+}
+
+// Example usage:
+
 </style>

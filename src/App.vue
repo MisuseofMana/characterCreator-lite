f5048fbd4d046f7dea484bd4fb87f65c748ca936
Sean Yager <contact@seanyager.com>
2021-02-19 17:28:02 -0600

    new version

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <b-row class="mb-3">
         <b-col>
           <h1 class="responsiveFont">KINGLOYAL ADVENTURER GENERATOR</h1>
-          <p class="tiny">V.0.4. Created by Sean Yager, &copy; 2021, All rights reserved.</p>
+          <p class="tiny">V.0.6. Created by Sean Yager, &copy; 2021, All rights reserved.</p>
         </b-col>
       </b-row>
 
